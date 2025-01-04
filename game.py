46c7b00731f241fcbcd8af1c1bd47c0f45c11ca5
Maija Laine <maijaelina05@gmail.com>
2025-01-04 12:57:24 +0200

    check game end states

--- game.py
+++ game.py
@@ -21,8 +21,11 @@ attempts = []
 result = ['B', 'B', 'B', 'B', 'B']
 results = []
 
+target_word = ['B', 'E', 'A', 'N', 'S']
+won = False
+lost = False
+
 def check_for_matches(_letters):
-    target_word = ['B', 'E', 'A', 'N', 'S']
     result = []
     for index, letter in enumerate(_letters):
         if letter == target_word[index]:
@@ -33,13 +36,23 @@ def check_for_matches(_letters):
             result.append('B')
     return result
 
+def check_if_won():
+    if attempts[attempt] == target_word:
+        return True
+    return False
+
+def check_if_lost():
+    if attempt == 4 and not won:
+        return True
+    return False
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
         
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and not lost and not won:
             if len(letters) > 0 and event.key == pygame.K_BACKSPACE:
                 letters.pop(len(letters)-1)
             elif len(letters) == max_length and event.key == pygame.K_RETURN:
@@ -47,6 +60,8 @@ while True:
                 attempts.append(letters[:])
                 results.append(result)
                 letters = []
+                won = check_if_won()
+                lost = check_if_lost()
                 attempt += 1
             elif event.unicode.isalpha() and len(letters) < max_length:
                 letters.append(event.unicode.upper())
@@ -57,47 +72,54 @@ while True:
     rect_height = SCREEN_WIDTH / 6
     spacing = (SCREEN_WIDTH - rect_width * 5) // 6
 
-    for x in range(5):
-        for y in range(5):
-            color = 'lightgrey'
-            
-            if y < len(results):
-                if results[y][x] == 'G':
-                    color = 'lightgreen'
-                elif results[y][x] == 'Y':
-                    color = 'lightyellow'
+    if not won and not lost:
+        for x in range(5):
+            for y in range(5):
+                color = 'lightgrey'
+                
+                if y < len(results):
+                    if results[y][x] == 'G':
+                        color = 'lightgreen'
+                    elif results[y][x] == 'Y':
+                        color = 'lightyellow'
 
-            pygame.draw.rect(
-                screen,
-                color,
-                pygame.Rect(
-                    spacing + x * (rect_width + spacing),
-                    spacing + y * (rect_height + spacing),
-                    rect_width,
-                    rect_height
-                ),
-                0,
-                5
-            )
+                pygame.draw.rect(
+                    screen,
+                    color,
+                    pygame.Rect(
+                        spacing + x * (rect_width + spacing),
+                        spacing + y * (rect_height + spacing),
+                        rect_width,
+                        rect_height
+                    ),
+                    0,
+                    5
+                )
 
-    for index, letter in enumerate(letters):
-        text1 = font1.render(letter, True, "black")
-        textRect1 = text1.get_rect()
-        textRect1.center = (
-            spacing + (rect_width / 2) + index * (rect_width + spacing),
-            spacing + (rect_height / 2) + attempt * (rect_height + spacing)
-        )
-        screen.blit(text1, textRect1)
-
-    for y, attempt_letters in enumerate(attempts):
-        for x, letter in enumerate(attempt_letters):
+        for index, letter in enumerate(letters):
             text1 = font1.render(letter, True, "black")
             textRect1 = text1.get_rect()
             textRect1.center = (
-                spacing + (rect_width / 2) + x * (rect_width + spacing),
-                spacing + (rect_height / 2) + y * (rect_height + spacing)
+                spacing + (rect_width / 2) + index * (rect_width + spacing),
+                spacing + (rect_height / 2) + attempt * (rect_height + spacing)
             )
             screen.blit(text1, textRect1)
+
+        for y, attempt_letters in enumerate(attempts):
+            for x, letter in enumerate(attempt_letters):
+                text1 = font1.render(letter, True, "black")
+                textRect1 = text1.get_rect()
+                textRect1.center = (
+                    spacing + (rect_width / 2) + x * (rect_width + spacing),
+                    spacing + (rect_height / 2) + y * (rect_height + spacing)
+                )
+                screen.blit(text1, textRect1)
+    elif won:
+        text_surface = font1.render('You win!', False, "black")
+        screen.blit(text_surface, (20, SCREEN_HEIGHT / 3))
+    else:
+        text_surface = font1.render('You lost!', False, "black")
+        screen.blit(text_surface, (20, SCREEN_HEIGHT / 3))
 
     pygame.display.flip()
     clock.tick(60)
